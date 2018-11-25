5902e25e2fba5f6ce03bc7ec3b37e6bd2629bbc8
Kieran Prasch <kieran@nucypher.com>
2018-11-25 14:46:59 -0800

    FleetState CLI status display bug fix when there are no known nodes

--- nucypher/characters/base.py
+++ nucypher/characters/base.py
@@ -171,6 +171,9 @@ class Character(Learner):
                     error = "Federated-only Characters derive their address from their Signing key; got {} instead."
                     raise self.SuspiciousActivity(error.format(checksum_address))
 
+        #
+        # Nicknames
+        #
         try:
             self.nickname, self.nickname_metadata = nickname_from_seed(self.checksum_public_address)
         except SigningPower.not_found_error:
@@ -179,6 +182,9 @@ class Character(Learner):
             else:
                 raise
 
+        #
+        # Fleet state
+        #
         if is_me is True:
             self.known_nodes.record_fleet_state()
 
