f991da628ec623a9d879c1711f6592cab3f13935
David Núñez <david@nucypher.com>
2019-01-19 13:38:14 +0100

    Use only 1 ursula in the policy

--- examples/heartbeat_demo/alicia.py
+++ examples/heartbeat_demo/alicia.py
@@ -118,7 +118,7 @@ doctor_strange = Bob.from_public_keys(powers_and_material=powers_and_material,
 policy_end_datetime = maya.now() + datetime.timedelta(days=5)
 # - m-out-of-n: This means Alicia splits the re-encryption key in 5 pieces and
 #               she requires Bob to seek collaboration of at least 3 Ursulas
-m, n = 3, 5
+m, n = 1, 1
 
 
 # With this information, Alicia creates a policy granting access to Bob.
