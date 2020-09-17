85f0884f74e000aa24bcc5d3d3010e04accd58ea
David Núñez <david@nucypher.com>
2020-09-17 22:18:38 +0200

    Some janitorial stuff

--- nucypher/config/storages.py
+++ nucypher/config/storages.py
@@ -201,7 +201,7 @@ class ForgetfulNodeStorage(NodeStorage):
         # Certificates
         self.__certificates = dict()
         self.__temporary_certificates = list()
-        self._temp_certificates_dir = tempfile.mkdtemp(prefix='nucypher-temp-certs-', dir=parent_dir)
+        self._temp_certificates_dir = tempfile.mkdtemp(prefix=self.__base_prefix, dir=parent_dir)
 
     @property
     def source(self) -> str:
@@ -448,11 +448,6 @@ class LocalFileBasedNodeStorage(NodeStorage):
         filepath = self.__generate_metadata_filepath(checksum_address=address, metadata_dir=filepath)
         self.__write_metadata(filepath=filepath, node=node)
         return filepath
-
-    def save_node(self, node, force) -> Tuple[str, str]:
-        certificate_filepath = self.store_node_certificate(certificate=node.certificate, force=force)
-        metadata_filepath = self.store_node_metadata(node=node)
-        return metadata_filepath, certificate_filepath
 
     @validate_checksum_address
     def remove(self, checksum_address: str, metadata: bool = True, certificate: bool = True) -> None:
